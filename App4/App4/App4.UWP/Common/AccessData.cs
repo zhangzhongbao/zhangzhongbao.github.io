4cfe6955e326c6414f6ad37875e3c355da98091d
张忠豹 <SY2013ppnn@hotmail.com>
2018-03-05 16:09:49 +0800

    333

--- App4/App4/App4.UWP/Common/AccessData.cs
+++ App4/App4/App4.UWP/Common/AccessData.cs
@@ -26,7 +26,7 @@ namespace SQLiteCommon
                 }
                 catch (HttpRequestException ex)
                 {
-                    //return ""; 
+                    return "";
                     ////throw new TrackSeriesApiException("", false, ex);
                 }
             }
